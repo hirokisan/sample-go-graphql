99a49a89d606c5273151969bcfc3c4abfdb8f3ff
hirokisan <expsharetoworld@gmail.com>
2020-10-29 21:59:44 +0900

    feat: implement resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,14 @@
 package graph
 
+import (
+	"github.com/hirokisan/sample-go-graphql/graph/model"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+// Resolver :
+type Resolver struct {
+	todos []*model.Todo
+}
