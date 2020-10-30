48a1840598b028caaf1d7a0aafe2b5875277c645
hirokisan <expsharetoworld@gmail.com>
2020-10-30 20:23:11 +0900

    test: create todo

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/99designs/gqlgen v0.13.0
+	github.com/stretchr/testify v1.4.0
 	github.com/vektah/gqlparser/v2 v2.1.0
+	google.golang.org/grpc v1.33.1
 )
---
+++ graph/schema.resolvers_test.go
@@ -0,0 +1,43 @@
+package graph
+
+import (
+	"testing"
+
+	"github.com/99designs/gqlgen/client"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/hirokisan/sample-go-graphql/graph/generated"
+	"github.com/hirokisan/sample-go-graphql/graph/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTodo(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		user := model.User{
+			ID: "1",
+		}
+		todos := []*model.Todo{
+			{
+				ID:   "1",
+				User: &user,
+			},
+			{
+				ID:   "2",
+				User: &user,
+			},
+		}
+		resolvers := Resolver{todos: todos}
+		c := client.New(handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers})))
+
+		var resp struct {
+			Todos []model.Todo
+		}
+		q := `
+    query findTodos {
+      todos {
+        id
+      }
+    }`
+		c.MustPost(q, &resp)
+		assert.Equal(t, len(todos), len(resp.Todos))
+	})
+}
